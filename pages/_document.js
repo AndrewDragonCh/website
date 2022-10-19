68cfd96bfe1584bca9bf5b84aa34a550297d3119
AndrewDragon <ats.gcs.2016@gmail.com>
2022-10-19 19:35:58 -0400

    added google tag manager for analytics

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PWGKXT9');</script>`,
+          }} />
         <meta name="description" content="AndrewDragon - YouTube Streamer and Content Creator" />
         <meta name="keywords" content="youtube, twitch, twitter" />
         <meta name="robots" content="index, follow" />
@@ -35,8 +43,13 @@ export default function Document() {
         <meta property='og:url' content='https://www.andrewdragon.dev/' />
       </Head>
       <body>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FX61WB9PER"></script>
         <Main />
         <NextScript />
+        <script dangerouslySetInnerHTML={{
+          __html: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWGKXT9"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`,
+        }} />
       </body>
     </Html>
   )
