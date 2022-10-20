5946cfa2332c9b604a0350a90bbf547f67a006d7
AndrewDragon <ats.gcs.2016@gmail.com>
2022-10-19 21:55:50 -0400

    Animated error 404 and 500 pages

--- components/header.js
+++ components/header.js
@@ -1,9 +1,6 @@
 import Head from 'next/head'
-import Script from 'next/script';
-import { useRouter } from 'next/router'
 
 export default function Header({pageTitle}) {
-  const router = useRouter()
   let fullPageTitle = 'AndrewDragon - ' + pageTitle;
   return (
     <Head>
--- pages/404.js
+++ pages/404.js
@@ -1,29 +1,29 @@
 import Header from '../components/header'
-import { useRouter } from 'next/router'
 import Link from 'next/link'
 import styles from '../styles/main.module.css'
+import 'animate.css'
 
 export default function Error404() {
-  const router = useRouter()
   return (
     <div className={styles.container}>
-      <Header pageTitle="Error 404" />
+      <Header pageTitle="Error 500" />
 
       <main className={styles.main}>
         <div className={styles.head}>
-          <h1 className={styles.title}>
+          <h1 className={`${styles.title} animate__animated animate__backInDown`}>
             Error
           </h1>
-          <h1 className={`${styles.title} ${styles.orange}`}>
+          <h1 className={`${styles.title} ${styles.orange} animate__animated animate__delay-1s animate__fadeInRight`}>
             404
           </h1>
         </div>
-
-        <p className={styles.description}>
-          <Link href="/" >
-            Page not Found
-          </Link>
-        </p>
+        <div className={`${styles.mainbody} animate__animated animate__delay-2s animate__fadeInUp`}>
+          <p className={`${styles.subtitle} animate__animated animate__fadeInUp animate__delay-2s`}>
+            <Link href="/" >
+              Page Not Found
+            </Link>
+          </p>
+        </div>
       </main>
     </div>
   )
