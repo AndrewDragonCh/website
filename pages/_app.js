bf6e08341bccacfc860b2a88b520100275fdc57b
Dragonaere <andrew@andrewdragon.dev>
2023-02-24 20:41:01 -0500

    Added Fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,37 @@
 import '../styles/globals.css'
 import { useEffect } from "react"
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   useEffect(() => {
-    if("serviceWorker" in navigator) {
-      window.addEventListener("load", function () {
-       navigator.serviceWorker.register("/sw.js").then(
-          function (registration) {
-            console.log("Service Worker registration successful with scope: ", registration.scope);
-          },
-          function (err) {
-            console.log("Service Worker registration failed: ", err);
-          }
-        );
-      });
+    Fathom.load('WAFSB', {
+      includedDomains: ['andrewdragon.dev'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+      if("serviceWorker" in navigator) {
+        window.addEventListener("load", function () {
+          navigator.serviceWorker.register("/sw.js").then(
+            function (registration) {
+              console.log("Service Worker registration successful with scope: ", registration.scope);
+            },
+            function (err) {
+              console.log("Service Worker registration failed: ", err);
+            }
+          );
+        });
+      }
     }
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
   }, [])
   return  (
     <>
