8db3735c6d70147d6d8394f05b8298240551b23c
Dragonaere <andrew@andrewdragon.dev>
2023-03-02 16:52:49 -0500

    Removed Fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,22 @@
 import '../styles/globals.css'
 import { useEffect } from "react"
-import { useRouter } from 'next/router';
-import * as Fathom from 'fathom-client';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
   useEffect(() => {
-    Fathom.load('WAFSB', {
-      includedDomains: ['stats.andrewdragon.dev'],
-    });
-
-    function onRouteChangeComplete() {
-      Fathom.trackPageview();
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
     }
-    router.events.on('routeChangeComplete', onRouteChangeComplete);
-
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete);
-    };
-  }, [router.events])
+  }, [])
   return  (
     <>
       <Head>
