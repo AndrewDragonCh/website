7b08bc822d04d1711ae8a6a91f983068f9cd5feb
AndrewDragon <astill@dragonaere.com>
2024-04-29 18:10:30 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.static.dragonaere.com'
+      },
+    ],
+  },
   output: 'export',
   poweredByHeader: false,
   reactStrictMode: true,
